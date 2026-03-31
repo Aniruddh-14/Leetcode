aa74b519750a49e65fdb50424ab803a0d46f11b7
Aniruddh-14 <aniruddhsharma081@gmail.com>
2026-03-31 23:14:26 +0530

    Time: 4 ms (70.28%) | Memory: 19.4 MB (93.77%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 
         res = []
-        candidates.sort() # Crucial for handling duplicates
+        candidates.sort() 
 
         def backtrack(start, target, path):
             if target == 0:
@@ -10,20 +10,16 @@ class Solution:
                 return
             
             for i in range(start, len(candidates)):
-                # If the current number is greater than the remaining target, 
-                # no need to check further (since the array is sorted)
                 if candidates[i] > target:
                     break
                 
-                # Skip duplicates: if the current element is the same as the 
-                # previous one in the same recursive level
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
                 
                 path.append(candidates[i])
-                # i + 1 because each number can be used only once
                 backtrack(i + 1, target - candidates[i], path)
-                path.pop() # Clean up for the next iteration
+                path.pop()
+                
 
         backtrack(0, target, [])
         return res
