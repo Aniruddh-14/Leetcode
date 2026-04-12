d81edb611c9e628510edaadf348607cccb14a117
Aniruddh-14 <aniruddhsharma081@gmail.com>
2026-04-12 19:02:34 +0530

    Time: 39 ms (45.66%) | Memory: 19.1 MB (94.21%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        n, m = len(num1), len(num2)
+        res = [0] * (n + m)
+        
+        # multiply from right to left
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                
+                # positions
+                p1 = i + j
+                p2 = i + j + 1
+                
+                total = mul + res[p2]
+                
+                res[p2] = total % 10      # unit digit
+                res[p1] += total // 10    # carry
+        
+        # convert to string (skip leading zeros)
+        result = ''.join(map(str, res)).lstrip('0')
+        
+        return result
